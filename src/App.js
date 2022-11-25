b969e3280d1bb2a07a97cdbe767bd212e621a1c4
zedan <you@example.com>
2022-11-25 18:48:49 +0530

    okie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import ContactApp from './ContactApp/ContactApp'
+import Home from './ContactApp/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+class App extends React.Component {
+    render() {
+        return <React.Fragment>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<ContactApp />} />
+                </Routes>
+            </Router>
+        </React.Fragment>
+    }
 }
-
-export default App;
+export default App
